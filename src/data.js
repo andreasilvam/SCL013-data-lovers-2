6f47121b4309d095bfea05eada7bbfcf82880e57
andreasilvam <andreasilvam@users.noreply.github.com>
2020-06-02 17:58:48 -0400

    subir cambios en html, js y css

--- src/data.js
+++ src/data.js
@@ -1,9 +1,38 @@
 // estas funciones son de ejemplo
+//`filterData(data, condition)`
+//`sortData(data, sortBy, sortOrder)`
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+export const filterData = (data, condition) => {
+  //console.log(data)
+  //console.log(condition)
+  //declaro variable tipo arreglo vacio, para que me guarde info
+  let houses = []
+  //recorro data
+for (let i=0; i < data.length; i++){
+
+  if (data[i].house === condition) {
+  //console.log(data[i].house)
+  //push para pasarle info al array vacio
+  houses.push(data[i]);
+}
+}
+//console.log(houses)
+return houses; //retorna arreglo con casas
+}
+
+//filtro los objetos de la data*/
+
+/*const search = data.filter(function (data) {
+  return data.house === condition;
+});
+return search;
+}*/
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterData } from './data.js';
+import dataPotter from './data/potter/potter.js';
+//console.log(example, data);
+//console.log(dataPotter);
 
-console.log(example, data);
+//IMPRIMIR DATA
+//constante que contiene información del atributo ID donde se va a imprimir en el html
+const root = document.getElementById("root");
+//for que recorrerá el largo de la data
+for (let i = 0; i < dataPotter.length; i++) {
+  //imprimir en html
+  root.innerHTML += `<section class="tarjetas">`
+   + `<img src="${dataPotter[i].image}" class="foto">` +
+   `<p> <section id="name">${dataPotter[i].name}</p>`
+}
+
+//FILTRAR POR CASA
+//constante para guardar ID llamado desde el DOM
+const selectHouse = document.getElementById("select");
+//llamando la constante para que cuando escuche el evento ejecute la función
+selectHouse.addEventListener("change", () => {
+  //variable que almacena el valor de la opción seleccionada por el usuario
+  let selectUser = selectHouse.options[selectHouse.selectedIndex].value;
+  //se llama a la constante que donde se imprime en el html y se declara vacia para que limpie la página
+  root.innerHTML = "";
+  //llamo a la funcion exportada desde main.js y guardo en una variable el llamado de la funcion, el resultado desde data js
+  let filterUser = filterData(dataPotter, selectUser); //esta variable que guardo en arreglo debe pasar por cada casa
+//console.log(filterUser)
+for (let i = 0; i< filterUser.length; i++){
+  root.innerHTML += `<section class="tarjetas">`
+  + `<img src="${filterUser[i].image}" class="foto">` +
+  `<p> <section id="name">${filterUser[i].name}</p>`
+}
+})
